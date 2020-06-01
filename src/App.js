5554a0c51da7237f95248faa66d0a2b9f6a5ec25
Grace <gb4yy@virginia.edu>
2020-06-01 15:38:15 -0400

    basic style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div id="main_header" class="main_header">
+          <button aria-label="Menu" class="menu"> Menu </button>
+          <a class="bigname"
+            href="//www.wsj.com">
+            <h1>THE WALL STREET JOURNAL.</h1>
+          </a>
+          <a class="smallname" href="//www.wsj.com">
+            <h1> WSJ</h1>
+          </a>
+          <button aria-label="Search" class="search" > Search </button>
+        </div>
+
+
+        <div className="subheader">
+          <div class="language_dropdown">
+            English Edition
+          </div>
+          <div class="date">
+            <p class="">June 1, 2020</p>
+          </div>
+          <div>
+            <a class="print_edition"
+              href="https://www.wsj.com/itp?mod=wsjheader">Print Edition</a>
+          </div>
+          <div><a class="video"
+            href="https://www.wsj.com/video?mod=wsjheader">Video</a>
+          </div>
+          <div>
+            <a class="latest_headlines"
+              href="https://www.wsj.com/news/latest-headlines?mod=wsjheader">
+              Latest Headlines</a>
+          </div>
+        </div>
+
       </header>
+
+      <body>
+        <p> articles go here </p>
+      </body>
     </div>
   );
 }
